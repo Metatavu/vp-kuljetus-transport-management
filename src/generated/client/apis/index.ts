aa2bd915586ec64cba7c0814177e641e10c97350
Eetu Lepistö <bodom02@gmail.com>
2024-02-06 22:14:56 +0200

    Added api client

---
+++ src/generated/client/apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DriversApi';
+export * from './RoutesApi';
+export * from './TowablesApi';
+export * from './TrucksApi';
+export * from './VehiclesApi';
---
+++ src/generated/client/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Driver';
+export * from './ModelError';
+export * from './Route';
+export * from './Towable';
+export * from './Truck';
+export * from './Vehicle';
