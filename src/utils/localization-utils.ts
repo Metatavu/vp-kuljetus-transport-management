047a256353ba32272006d8c3e87f3227053d8d4a
Ville Juutila <ville.juutila@gmail.com>
2024-02-26 14:26:02 +0200

    Fixed broken build

--- src/utils/localization-utils.ts
+++ src/utils/localization-utils.ts
@@ -2,10 +2,10 @@ import { TaskType } from "generated/client";
 import { LocalizedLabelKey } from "src/types";
 
 namespace LocalizationUtils {
-  export const getLocalizedTaskType = (type: TaskType): LocalizedLabelKey => ({
-    LOAD: "drivePlanning.tasks.loadTaskType",
-    UNLOAD: "drivePlanning.tasks.unloadTaskType",
-  } as Record<TaskType, LocalizedLabelKey>)[type];
+  export const getLocalizedTaskType = (type: TaskType): LocalizedLabelKey<"drivePlanning.tasks"> => ({
+    LOAD: "loadTaskType",
+    UNLOAD: "unloadTaskType",
+  } as Record<TaskType, LocalizedLabelKey<"drivePlanning.tasks">>)[type];
 }
 
 export default LocalizationUtils;
