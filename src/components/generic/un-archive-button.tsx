641ea59a50fd426285087fa4ea64f67d94178f4d
Simeon <simeon.platonov@gmail.com>
2025-05-07 14:36:01 +0300

    Added an option to unarchive users

---
+++ src/components/generic/un-archive-button.tsx
@@ -0,0 +1,42 @@
+import { Replay } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { useConfirmDialog } from "components/providers/confirm-dialog-provider";
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+
+type Props = {
+  title: string;
+  description: string;
+  onUnArchive: () => void;
+};
+
+const UnArchiveButton = ({ title, description, onUnArchive }: Props) => {
+  const { t } = useTranslation();
+  const showConfirmDialog = useConfirmDialog();
+
+  const onButtonClick = useCallback(
+    () =>
+      showConfirmDialog({
+        title: title,
+        description: description,
+        positiveButtonColor: "info",
+        positiveButtonText: t("unArchive"),
+        onPositiveClick: onUnArchive,
+      }),
+    [t, title, description, onUnArchive, showConfirmDialog],
+  );
+
+  return (
+    <Button
+      variant="text"
+      color="info"
+      startIcon={<Replay />}
+      sx={{ alignSelf: "flex-end" }}
+      onClick={onButtonClick}
+    >
+      {t("unArchive")}
+    </Button>
+  );
+};
+
+export default UnArchiveButton;
